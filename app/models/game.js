71c96033f1d396a6e87ae392032ed10ac1c6fef2
DeliriumNocturnum <jake@schwartzpub.com>
2015-03-15 18:56:24 -0500

    missing files 2 electric boogaloo

---
+++ app/models/game.js
@@ -0,0 +1,17 @@
+// app/models/game.js
+// load the things we need
+var mongoose = require('mongoose');
+
+// define the schema for our game model
+var gameSchema = mongoose.Schema({
+	room		: String,
+	status		: String,
+	numPlayers	: Number,
+	players		: [],
+	winner		: String,
+	start		: Date,
+	end			: Date
+});
+
+// create the model for games and expose it to our app
+module.exports = mongoose.model('Game', gameSchema);
